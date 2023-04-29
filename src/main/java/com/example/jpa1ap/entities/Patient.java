46adb327e14848f0ac3ee94b8d0cd6473de7a0eb
LorigaSoufiane <soufiane.loriga1@gmail.com>
2023-04-29 14:37:17 +0100

    Fc

---
+++ src/main/java/com/example/jpa1ap/entities/Patient.java
@@ -0,0 +1,32 @@
+package com.example.jpa1ap.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Patient {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    @Size(min = 4, max = 40)
+    @Column(length = 50)
+    private String nom;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dateNaissance;
+    private boolean malade;
+    @DecimalMin("10")
+    private int score;
+
+
+}
---
+++ src/test/java/com/example/jpa1ap/Jpa1ApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa1ap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa1ApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
